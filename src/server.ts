6a45131c31ffc16ae527a6b1157655ca973d50d4
Matheus Rodrigues dos Santos <matheus.rodrisantos@outlook.com>
2023-09-13 06:50:19 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import {fastify} from 'fastify'
+import { prisma } from './lib/prisma'
+
+const app = fastify()
+
+app.get('/prompts',async ()=>{
+
+    const prompts =await prisma.prompt.findMany()
+
+    return prompts
+})
+
+app.listen({
+    port:3333
+}).then(()=>{
+    console.log('HTTP Server running ! ')
+})
