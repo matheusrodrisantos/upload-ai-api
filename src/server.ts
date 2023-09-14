efad352c94fb3b081a853471cef455db7a2346a1
Matheus Rodrigues dos Santos <matheus.rodrisantos@outlook.com>
2023-09-14 00:05:24 -0300

    upload de videos, trasncription

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import {fastify} from 'fastify'
 import { prisma } from './lib/prisma'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
 
-app.get('/prompts',async ()=>{
-
-    const prompts =await prisma.prompt.findMany()
-
-    return prompts
-})
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
     port:3333
